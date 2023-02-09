f15e58d3650525f114fadbea6d25f7b90a834494
Mehulparekh144 <mehulparekh144@gmail.com>
2023-02-09 23:57:23 +0530

    first commit

---
+++ src/components/Social.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div>
+          <div className='links d-flex-col my-2 justify-content-center'>
+                    
+                    <div className='link'><a href='https://github.com/Mehulparekh144'z target="_blank"><i className="fa-brands fa-github"></i></a></div>
+                    <div className='link'><a href='https://www.linkedin.com/in/mehul-parekh-a519a9196/' target="_blank" ><i className="fa-brands fa-linkedin-in"></i></a></div>
+                    <div className='link'><a href='/' target="_blank"><i className="fa-brands fa-instagram"></i></a></div>
+                </div>
+      
+    </div>
+  )
+}
+
+export default Social
